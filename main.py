62ed93def96bced3204c70a21b1b549b8465776a
Turner3800 <42954322+Turner3800@users.noreply.github.com>
2020-12-27 20:28:28 -1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8934y5uihkbjvfuyoiy4p3o85ugyihjvsb9a75bwefigu374tgf'
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app)
