583bcee25d669fc241e7adaf509d64b3b02ee899
Turner3800 <42954322+Turner3800@users.noreply.github.com>
2020-12-29 08:59:29 -1000

    Update main.py
    added socketio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8934y5uihkbjvfuyoiy4p3o85ugyihjvsb9a75bwefigu374tgf'
@@ -7,7 +7,11 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template("index.html")
+
+@socketio.on('message')
+def message(data):
+	send(data)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
