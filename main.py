8c9157905b42cc2942edca10dde9239f3ea3c985
Turner3800 <42954322+Turner3800@users.noreply.github.com>
2020-12-31 19:51:36 -1000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, request, url_for
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8934y5uihkbjvfuyoiy4p3o85ugyihjvsb9a75bwefigu374tgf'
+app.secret_key = "ARK9999999995999"
 socketio = SocketIO(app)
 
-@app.route('/')
+
+@app.route('/', methods = ['GET', 'POST'])
+def username():
+	#if the user presses the submit button on username screen
+	if request.method == "POST":
+		username = request.form["usr"]
+		session["username"] = username
+		if session["username"] != "":
+			return redirect(url_for("home"))
+	return render_template("username.html")
+
+@app.route('/home')
 def home():
-	return render_template("index.html")
+	return render_template("home.html", username=session["username"])
+
+#@app.route("/audio_stream")
+#def Audio_Stream():
+    #r = requests.get("http://localhost:8082/audio_stream.mp3", stream=True)
+    #return Response(r.iter_content(chunk_size=1024), mimetype='audio/mpeg')
 
 @socketio.on('message')
 def message(data):
+	print(data)
 	send(data)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
